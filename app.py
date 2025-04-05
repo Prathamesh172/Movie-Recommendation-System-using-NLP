98903bb2eb19ef6943ce202f0f85519a28af84c9
Prathamesh Ugle <163085808+Prathamesh172@users.noreply.github.com>
2025-04-05 19:49:35 +0530

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+nltk.data.path.append('./nltk_data')
 nltk.download('punkt')
 nltk.download('stopwords')
 

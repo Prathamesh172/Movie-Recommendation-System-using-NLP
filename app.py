d11a00595ed02c777bc38ee0e45f301fc5550361
Prathamesh Ugle <163085808+Prathamesh172@users.noreply.github.com>
2025-04-05 22:16:43 +0530

    app.py

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def home():
             return render_template('index.html', error="An error occurred!")
     return render_template('index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render will inject the PORT variable
+    app.run(host="0.0.0.0", port=port)
+
